33e201374412f357dc13a8576399aad148299ebc
Branko Vukelic <branko@outernet.is>
2016-01-14 22:51:15 +0100

    Added better version comparison
    The new version comparison code takes into account suffixes like -a1, -rc2 etc,
    and can compare versions with variable number of minor and patch digits.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rootfs/pkg/installer.sh
+++ rootfs/pkg/installer.sh
@@ -4,6 +4,65 @@ INSTALLER=$1
 
 CONTENTS=$($INSTALLER --list)
 
+SFXMAJ_FAC=100
+PAT_FAC=1000
+MIN_FAC=1000000
+MAJ_FAC=1000000000
+
+hasfile() {
+  filename=$1
+  echo $CONTENTS | grep "$filename" 2>&1 >/dev/null
+}
+
+suffix() {
+  sfx=$(echo "$1" | cut -d- -f2)
+  if [ "$sfx" == "$1" ]; then
+    echo "z1"
+  else
+    echo $sfx
+  fi
+}
+
+mainver() {
+  echo "$1" | cut -d- -f1
+}
+
+normalize() {
+  # Convert a version number to a single integer
+  ver=$(mainver $1)
+  sfx=$(suffix $1)
+  sfxtype=$(echo $sfx | egrep -o "[a-z]+")
+  sfxnum=$(echo $sfx | egrep -o "[0-9]+")
+  vmaj=$(echo "$ver" | cut -d. -f1)
+  vmin=$(echo "$ver" | cut -d. -f2)
+  vpat=$(echo "$ver" | cut -d. -f3)
+
+  if [ -z $vpat ]; then
+    vpat=0
+  fi
+
+  case "$sfxtype" in
+    a)
+      sfxmaj=1
+      ;;
+    b)
+      sfxmaj=2
+      ;;
+    rc)
+      sfxmaj=3
+      ;;
+    *)
+      sfxmaj=4
+      ;;
+  esac
+
+  echo $(expr $MAJ_FAC \* $vmaj + $MIN_FAC \* $vmin + $PAT_FAC \* $vpat + $SFXMAJ_FAC \* $sfxmaj + $sfxnum)
+}
+
+vercmp() {
+  test $(normalize $1) -gt $(normalize $2)
+}
+
 # Platform check
 if [ "$(cat /etc/platform 2> /dev/null)" != "wt200" ]; then
   echo "Incorrect platform"
@@ -11,30 +70,25 @@ if [ "$(cat /etc/platform 2> /dev/null)" != "wt200" ]; then
 fi
 
 # Check for version if present
-if echo $CONTENTS | grep version > /dev/null ; then
+if hasfile version; then
   PKG_VER=$($INSTALLER --extract version -)
   OS_VER=$(cat /etc/version 2> /dev/null)
 
-  if [ "$PKG_VER" == "$OS_VER" ]; then
-    echo "Package already installed"
-    exit 1
-  fi
-  CHK=$(echo -e "$PKG_VER\n$OS_VER\n" | sort -r | head -1)
-  if [ "$PKG_VER" != "$CHK" ]; then
-    echo "Package version is older than installed version"
+  if ! vercmp "$PKG_VER" "$OS_VER"; then
+    echo "Package already installed or newer version installed"
     exit 1
   fi
 fi
 
 # look for a pre-update script
-if echo $CONTENTS | grep pre-install.sh > /dev/null ; then
+if hasfile pre-install.sh; then
   $INSTALLER --extract pre-install.sh /tmp
   [ -x /tmp/pre-install.sh ] && ( /tmp/pre-install.sh $INSTALLER || exit 1 )
   rm /tmp/pre-install.sh
 fi
 
 # Install kernel
-if echo $CONTENTS | grep kernel.img > /dev/null ; then
+if hasfile kernel.img; then
   MTD_BOOT=$(grep boot /proc/mtd | sed 's/^mtd\([0-9]\+\).*/\1/')
   MTD_RECOVERY=$(grep recovery /proc/mtd | sed 's/^mtd\([0-9]\+\).*/\1/')
   if [ -z "$MTD_BOOT" ] || [ -z "$MTD_RECOVERY" ]; then
@@ -46,12 +100,12 @@ if echo $CONTENTS | grep kernel.img > /dev/null ; then
 fi
 
 # Install rotfs
-if echo $CONTENTS | grep rootfs.ubifs > /dev/null ; then
+if hasfile rootfs.ubifs; then
   $INSTALLER --ubi rootfs.ubifs ubi0:rootfs || exit 1
 fi
 
 # u-boot
-if echo $CONTENTS | grep u-boot.bin > /dev/null ; then
+if hasfile u-boot.bin; then
   MTD_UBOOT=$(grep bootloader /proc/mtd | sed 's/^mtd\([0-9]\+\).*/\1/')
   if [ -z "$MTD_UBOOT" ]; then
     logger -s -t installer.sh "Could not determine u-boot mtd partitions"
@@ -61,7 +115,7 @@ if echo $CONTENTS | grep u-boot.bin > /dev/null ; then
 fi
 
 # look for a post-install script
-if echo $CONTENTS | grep post-install.sh > /dev/null ; then
+if hasfile post-install.sh; then
   $INSTALLER --extract post-install.sh /tmp
   [ -x /tmp/post-install.sh ] && ( /tmp/post-install.sh $INSTALLER || exit 1 )
   rm /tmp/post-install.sh
@@ -69,3 +123,5 @@ fi
 
 logger -s -t installer.sh "Installation complete"
 /sbin/reboot
+
+# vim: set tabstop=2 softtabstop=2 shiftwidth=2:
