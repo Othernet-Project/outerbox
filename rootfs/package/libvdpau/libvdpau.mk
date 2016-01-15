72262ca02a01b0b41810e9ac8fca1aa102e6795a
Andrean Franc <andrean.franc@gmail.com>
2016-01-15 22:08:34 +0100

    Add missing dependency libvdpau

---
+++ rootfs/package/libvdpau/libvdpau.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# libvdpau
+#
+#############################################################
+LIBVDPAU_VERSION = 1.1.1
+LIBVDPAU_SITE = http://people.freedesktop.org/~aplattner/vdpau
+LIBVDPAU_SOURCE = libvdpau-$(LIBVDPAU_VERSION).tar.gz
+LIBVDPAU_INSTALL_STAGING = NO
+LIBVDPAU_INSTALL_TARGET = YES
+LIBVDPAU_SECTION = multimedia
+LIBVDPAU_DESCRIPTION = a Video Decode and Presentation API for UNIX.
+LIBVDPAU_DEPENDENCIES = xlib_libX11
+LIBVDPAU_OPKG_DEPENDENCIES = libx11
+LIBVDPAU_CONF_OPTS = \
+  --disable-documentation \
+  --enable-dri2 \
+  --with-module-dir=/usr/lib/vdpau
+
+$(eval $(autotools-package))
