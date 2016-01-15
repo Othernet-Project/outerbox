97602c9c3ac62d4908a46f25ce9c8ce0ea67f027
Branko Vukelic <branko@outernet.is>
2016-01-15 13:00:39 +0100

    Updated librarian-ondd package
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rootfs/package/python-librarian-ondd/python-librarian-ondd.mk
+++ rootfs/package/python-librarian-ondd/python-librarian-ondd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_ONDD_VERSION = 4021c851ae6f7bdbd170348f77e7972293949c97
+PYTHON_LIBRARIAN_ONDD_VERSION = 610b329247e480314a86e298c51941106218a576
 PYTHON_LIBRARIAN_ONDD_SOURCE = $(PYTHON_LIBRARIAN_ONDD_VERSION).tar.gz
 PYTHON_LIBRARIAN_ONDD_SITE = https://github.com/Outernet-Project/librarian-ondd/archive
 PYTHON_LIBRARIAN_ONDD_LICENSE = GPL
