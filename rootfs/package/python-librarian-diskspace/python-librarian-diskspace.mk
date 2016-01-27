1e9efd6763cee115bca88cb4609e598aa7a4fcdf
Ben Ennis <ben@outernet.is>
2016-01-27 08:43:35 -0800

    set diskspace to latest master

--- rootfs/package/python-librarian-diskspace/python-librarian-diskspace.mk
+++ rootfs/package/python-librarian-diskspace/python-librarian-diskspace.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_DISKSPACE_VERSION = c073419b71d5a8eea8bd69a8c71dc5dfc1e75b8f
+PYTHON_LIBRARIAN_DISKSPACE_VERSION = e89d10587a812a3b241ebb29cda92f8f6f932cc1
 PYTHON_LIBRARIAN_DISKSPACE_SOURCE = $(PYTHON_LIBRARIAN_DISKSPACE_VERSION).tar.gz
 PYTHON_LIBRARIAN_DISKSPACE_SITE = https://github.com/Outernet-Project/librarian-diskspace/archive
 PYTHON_LIBRARIAN_DISKSPACE_LICENSE = GPL
