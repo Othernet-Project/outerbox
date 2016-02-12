9cc40d12fef3da4838463ada17d57595c9e74c0e
Branko Vukelic <branko@outernet.is>
2016-02-12 11:31:49 +0100

    Updated FSAL
    This version contains fixes for blacklist not working as intended, and also
    uses regex patterns for blacklisting.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rootfs/package/python-fsal/python-fsal.mk
+++ rootfs/package/python-fsal/python-fsal.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_FSAL_VERSION = 8af6335b03b777077d82afae7199279e31b8056f
+PYTHON_FSAL_VERSION = b32da3e4c9afae5d44367bbb829d567757778250
 PYTHON_FSAL_SOURCE = $(PYTHON_FSAL_VERSION).tar.gz
 PYTHON_FSAL_SITE = https://github.com/Outernet-Project/fsal/archive
 PYTHON_FSAL_LICENSE = GPL
