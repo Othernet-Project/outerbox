ff88d45cc883f683e428caf983d6ebefd3a14544
Branko Vukelic <branko@outernet.is>
2016-02-15 15:58:54 +0100

    Updated librarian-ui
    Adds the recompiled static assets
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rootfs/package/python-librarian-ui/python-librarian-ui.mk
+++ rootfs/package/python-librarian-ui/python-librarian-ui.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_UI_VERSION = 291e7681864441bd06ed464935eb19df3aff1f55
+PYTHON_LIBRARIAN_UI_VERSION = d0dd322bb36d07acafc1c9d0b5defc81558fe818
 PYTHON_LIBRARIAN_UI_SOURCE = $(PYTHON_LIBRARIAN_UI_VERSION).tar.gz
 PYTHON_LIBRARIAN_UI_SITE = https://github.com/Outernet-Project/librarian-ui/archive
 PYTHON_LIBRARIAN_UI_LICENSE = GPL
