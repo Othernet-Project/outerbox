4d45ee8299565738f62fc3c663aa06ac05300237
Branko Vukelic <branko@outernet.is>
2016-02-15 15:23:01 +0100

    Updated librarian-library
    Fixes references to default language setting.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rootfs/package/python-librarian-library/python-librarian-library.mk
+++ rootfs/package/python-librarian-library/python-librarian-library.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_LIBRARY_VERSION = 75174c430de5b702b6e0b97c4c9cf3dad54df9a3
+PYTHON_LIBRARIAN_LIBRARY_VERSION = f5e8372667b7e414d993f5af034b7c445c1b6890
 PYTHON_LIBRARIAN_LIBRARY_SOURCE = $(PYTHON_LIBRARIAN_LIBRARY_VERSION).tar.gz
 PYTHON_LIBRARIAN_LIBRARY_SITE = https://github.com/Outernet-Project/librarian-library/archive
 PYTHON_LIBRARIAN_LIBRARY_LICENSE = GPL
