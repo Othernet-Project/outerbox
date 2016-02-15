0c0871534ad3c50d990f8e77ca99885055571013
Branko Vukelic <branko@outernet.is>
2016-02-15 16:59:29 +0100

    Updated librarian components
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rootfs/package/python-librarian-settings/python-librarian-settings.mk
+++ rootfs/package/python-librarian-settings/python-librarian-settings.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_SETTINGS_VERSION = 79cb7f96159bb36fef50835eac088a8fdde87063
+PYTHON_LIBRARIAN_SETTINGS_VERSION = f7e972da1a00fd9c59b03e8c4ea598a2ac1d0ba8
 PYTHON_LIBRARIAN_SETTINGS_SOURCE = $(PYTHON_LIBRARIAN_SETTINGS_VERSION).tar.gz
 PYTHON_LIBRARIAN_SETTINGS_SITE = https://github.com/Outernet-Project/librarian-settings/archive
 PYTHON_LIBRARIAN_SETTINGS_LICENSE = GPL
