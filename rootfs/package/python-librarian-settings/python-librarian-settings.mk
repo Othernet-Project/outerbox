9247067c5a63923aa5fc9244b552659990dcb958
Branko Vukelic <branko@outernet.is>
2016-02-15 15:22:36 +0100

    Updated librarian-settings
    Styling fixes
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rootfs/package/python-librarian-settings/python-librarian-settings.mk
+++ rootfs/package/python-librarian-settings/python-librarian-settings.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_SETTINGS_VERSION = 2afd4a8545fdf6e8ed33dea2e557aa61b89ad98d
+PYTHON_LIBRARIAN_SETTINGS_VERSION = 79cb7f96159bb36fef50835eac088a8fdde87063
 PYTHON_LIBRARIAN_SETTINGS_SOURCE = $(PYTHON_LIBRARIAN_SETTINGS_VERSION).tar.gz
 PYTHON_LIBRARIAN_SETTINGS_SITE = https://github.com/Outernet-Project/librarian-settings/archive
 PYTHON_LIBRARIAN_SETTINGS_LICENSE = GPL
