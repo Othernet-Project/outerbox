d1e3acaa7da62bba369687e9c437803f1101ef4d
Branko Vukelic <branko@outernet.is>
2016-02-15 14:51:04 +0100

    Updated librarian-ui
    Fixes related to checkbox handling
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rootfs/package/python-librarian-ui/python-librarian-ui.mk
+++ rootfs/package/python-librarian-ui/python-librarian-ui.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_UI_VERSION = 2aa98d106093982f36d14196a254a3fad6ba444e
+PYTHON_LIBRARIAN_UI_VERSION = 1b92268964e8386e5e0e733e4211dbfe360ac35f
 PYTHON_LIBRARIAN_UI_SOURCE = $(PYTHON_LIBRARIAN_UI_VERSION).tar.gz
 PYTHON_LIBRARIAN_UI_SITE = https://github.com/Outernet-Project/librarian-ui/archive
 PYTHON_LIBRARIAN_UI_LICENSE = GPL
