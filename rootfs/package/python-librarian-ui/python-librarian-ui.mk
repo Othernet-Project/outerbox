70cffe1ee48579bd5ca14068ea6407ce71e9e8fc
Branko Vukelic <branko@outernet.is>
2016-02-15 15:23:57 +0100

    Updated librarian-ui
    Fixes label and checkbox rendering issues
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rootfs/package/python-librarian-ui/python-librarian-ui.mk
+++ rootfs/package/python-librarian-ui/python-librarian-ui.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_UI_VERSION = 1b92268964e8386e5e0e733e4211dbfe360ac35f
+PYTHON_LIBRARIAN_UI_VERSION = 291e7681864441bd06ed464935eb19df3aff1f55
 PYTHON_LIBRARIAN_UI_SOURCE = $(PYTHON_LIBRARIAN_UI_VERSION).tar.gz
 PYTHON_LIBRARIAN_UI_SITE = https://github.com/Outernet-Project/librarian-ui/archive
 PYTHON_LIBRARIAN_UI_LICENSE = GPL
