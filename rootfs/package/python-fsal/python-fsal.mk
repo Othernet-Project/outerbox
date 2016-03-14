e97b4299e2a6845d107bb0a8db9a29cef91afaa7
Branko Vukelic <branko@outernet.is>
2016-03-14 16:15:51 +0100

    Fixed packages and versions
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rootfs/package/python-fsal/python-fsal.mk
+++ rootfs/package/python-fsal/python-fsal.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_FSAL_VERSION = 58b26a82898c70f1fc1835a9eb467f0cd9e08a45
+PYTHON_FSAL_VERSION = 935b7fefbc5df3ca3d7e59709908a94f4e9e77e7
 PYTHON_FSAL_SITE = $(call github,Outernet-Project,fsal,$(PYTHON_FSAL_VERSION))
 PYTHON_FSAL_LICENSE = GPL
 PYTHON_FSAL_SETUP_TYPE = setuptools
--- rootfs/package/python-greentasks/python-greentasks.mk
+++ rootfs/package/python-greentasks/python-greentasks.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_GREENTASKS_VERSION = 58b26a82898c70f1fc1835a9eb467f0cd9e08a45
+PYTHON_GREENTASKS_VERSION = fabc18c1718dde3722ca76452aa211867361b908
 PYTHON_GREENTASKS_SITE = $(call github,Outernet-Project,greentasks,$(PYTHON_GREENTASKS_VERSION))
 PYTHON_GREENTASKS_LICENSE = GPL
 PYTHON_GREENTASKS_SETUP_TYPE = setuptools
