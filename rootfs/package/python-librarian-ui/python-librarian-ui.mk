9c60292ac345d2074cd709149ef3ae081b7c51d4
Branko Vukelic <branko@outernet.is>
2016-03-14 16:46:28 +0100

    Updated librarian-ui
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- rootfs/package/python-librarian-ui/python-librarian-ui.mk
+++ rootfs/package/python-librarian-ui/python-librarian-ui.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_LIBRARIAN_UI_VERSION = 7612cf49593396d82651cda62a42619b631b9eaa
+PYTHON_LIBRARIAN_UI_VERSION = 40f0117020d189ccbfa3e9ed9191f748daa1d132
 PYTHON_LIBRARIAN_UI_SITE = $(call github,Outernet-Project,librarian-ui,$(PYTHON_LIBRARIAN_UI_VERSION))
 PYTHON_LIBRARIAN_UI_LICENSE = GPL
 PYTHON_LIBRARIAN_UI_SETUP_TYPE = setuptools
